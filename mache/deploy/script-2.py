ab7ca2b9d494ade4741d3447f3b65c4acd5b8b8c
Althea Denlinger <altheaden@unm.edu>
2024-11-05 12:52:15 -0800

    Unify script 2 and shared

--- mache/deploy/script-2.py
+++ mache/deploy/script-2.py
@@ -3,8 +3,6 @@ import argparse
 import subprocess
 import sys
 
-from deploy.shared import check_call
-
 
 def main():
     # install conda
@@ -13,7 +11,6 @@ def main():
     #    need to parse arguments to install local vs conda mache ?
     # begin deployment
     #    send the rest of the arguments to the next script (bootstrap)
-    # could url retrieve files needed (such as shared)
     args = parse_args()
     print(args)
     options = vars(args)
@@ -39,6 +36,37 @@ def build_env(options):
         f'conda config --set channel_priority strict && ' \
         f'conda create -y -n begin_mache_deploy --file {options["spec_file"]}'
     check_call(commands)
+
+
+def check_call(commands, env=None, logger=None):
+    command_list = commands.replace(' && ', '; ').split('; ')
+    print_command = '\n   '.join(command_list)
+    if logger is None:
+        print(f'\n Running:\n   {print_command}\n')
+    else:
+        logger.info(f'\nrunning:\n   {print_command}\n')
+
+    if logger is None:
+        process = subprocess.Popen(commands, env=env, executable='/bin/bash',
+                                   shell=True)
+        process.wait()
+    else:
+        process = subprocess.Popen(commands, stdout=subprocess.PIPE,
+                                   stderr=subprocess.PIPE, env=env,
+                                   executable='/bin/bash', shell=True)
+        stdout, stderr = process.communicate()
+
+        if stdout:
+            stdout_decoded = stdout.decode('utf-8')
+            for line in stdout_decoded.split('\n'):
+                logger.info(line)
+        if stderr:
+            stderr_decoded = stderr.decode('utf-8')
+            for line in stderr_decoded.split('\n'):
+                logger.error(line)
+
+    if process.returncode != 0:
+        raise subprocess.CalledProcessError(process.returncode, commands)
 
 
 if __name__ == '__main__':
