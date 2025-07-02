aabb5035ab4719ff293c7b3d84bd08e9a10000de
Althea Denlinger <theabecca@gmail.com>
2025-07-02 12:55:43 -0500

    !fixup init

--- mache/spack/__init__.py
+++ mache/spack/__init__.py
@@ -1,17 +1,11 @@
-import os as os
-import subprocess as subprocess
-
-import yaml as yaml
-from jinja2 import Template as Template
-
-from mache.machine_info import (
-    MachineInfo as MachineInfo,
-)
-from mache.machine_info import (
-    discover_machine as discover_machine,
-)
 from mache.spack.config_machines import (
     config_to_shell_script as config_to_shell_script,
+)
+from mache.spack.config_machines import (
+    extract_machine_config as extract_machine_config,
+)
+from mache.spack.config_machines import (
+    extract_spack_from_config_machines as extract_spack_from_config_machines,
 )
 from mache.spack.env import (
     get_modules_env_vars_and_mpi_compilers as get_modules_env_vars_and_mpi_compilers,  # noqa: E501
